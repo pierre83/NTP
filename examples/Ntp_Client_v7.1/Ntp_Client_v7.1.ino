b9c8756c4d5d0da9fab5175614ad612d1226008c
pierre83 <pcasal@free.fr>
2020-04-25 07:06:40 +0200

    New examples

---
+++ examples/Ntp_Client_v7.1/Ntp_Client_v7.1.ino
@@ -0,0 +1,161 @@
+/*
+
+  NTP Client
+
+  Get the time from a Network Time Protocol (NTP) server
+
+  For more on NTP time servers and the messages needed to communicate with them,
+  see http://en.wikipedia.org/wiki/Network_Time_Protocol
+
+  February 2020 - Pierre Casal
+
+  ESP32  ->   W5500 -> ESP8266
+  GPIO19  --> MISO  -> D6
+  GPIO23  --> MOSI  -> D7
+  GPIO18  --> SCK   -> D5
+  GPIO05  --> CS    -> D2  (ESP32: GPIO04 et GPIO05 testées)
+*/
+
+#include <SPI.h>
+#include <Ethernet.h>
+#include <TimeLib.h>
+#include <Ntp.h>
+#include "..\tools\Ethernet_W5x00\Init_W5x00\Init_W5x00.ino"
+
+//------------------------------------------------------------
+// CHANGE AS DESIRED:
+const char timeServerName[] = "fr.pool.ntp.org";
+IPAddress timeServerIP = {192, 168, 0, 254 };
+
+
+#if defined (ESP8266)
+const uint8_t CS = 4;   // ESP8266 CS -> D2 (GPIO04)
+#elif defined (ESP32)
+const uint8_t CS = 4;     // ESP32   CS -> D4 (GPIO04(D4) et GPIO05(D5) testées
+#elif defined (__AVR__)
+const uint8_t CS = 10;  // AVR (default)
+#endif
+
+const uint8_t TIMEZONE = 1;
+
+uint8_t mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
+
+IPAddress ip(192, 168, 0, 56);
+IPAddress myDns(192, 168, 0, 254);
+
+bool dhcp = 1;  // 0= fixed IP, 1 = DHCP
+
+const int DNS_TIMEOUT = 2500;
+//------------------------------------------------------------
+
+
+NTPClient ntp;
+
+tmElements_t tm;
+
+
+
+void setup()
+{
+  // Open serial communications and wait for port to open:
+  Serial.begin(115200);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+
+  Ethernet.init(CS);
+
+  // start the Ethernet connection:
+  Ethernet_init( dhcp, mac, ip, myDns);
+
+  // Start NTP
+  ntp.begin(TIMEZONE, DNS_TIMEOUT);
+}
+
+void loop()
+{
+  Serial.println(F("\nGet time !"));
+  Serial.println(F("ntp.getEpoch(timeServerIP)"));
+  uint32_t debut = millis();
+  uint32_t epoch = ntp.getEpoch(timeServerIP); // send an NTP packet to a time server
+  uint32_t fin = millis();
+  Serial.print(F("Epoch: "));       Serial.println(epoch);
+  afficheTemps(fin - debut);
+
+  Serial.println(F("ntp.getEpoch(timeServerName)"));
+  debut = millis();
+  epoch = ntp.getEpoch(timeServerName); // Ask to a time server
+  Serial.print(F("Epoch: "));       Serial.println(epoch);
+  fin = millis();
+  afficheTemps(fin - debut);
+
+  Serial.print(F("Unix time = "));  Serial.println(epoch);
+
+  Serial.print(F("UTC time is "));       // UTC is the time at Greenwich Meridian (GMT)
+  afficheHeure(epoch);
+
+  Serial.println(F("\nntp.localTime(epoch)"));
+  debut = millis();
+  uint32_t epoch_here = ntp.localTime(epoch);
+  fin = millis();
+  Serial.print(F("Epoch: "));       Serial.println(epoch_here);
+  afficheTemps(fin - debut);
+
+  Serial.print(F("Epoch DST = "));
+  Serial.println(epoch_here);
+
+  Serial.print(F("Local time is "));       // Local time with time zone and DST
+  afficheHeure(epoch_here);
+  Serial.println(F("\nntp.localTime()"));
+  debut = millis();
+  epoch_here = ntp.localTime();
+  fin = millis();
+  afficheTemps(fin - debut);
+
+  Serial.print(F("Epoch DST = "));  Serial.println(epoch_here);
+
+  Serial.print(F("Local time is "));       // Local time with time zone and DST
+  afficheHeure(epoch_here);
+
+  bool dst = ntp.isDST(epoch);
+  if ( dst == true ) {
+    Serial.println(F("\nSummer time"));
+  } else {
+    Serial.println(F("\nWinter time"));
+  }
+
+  Serial.println(F("\nSocket Errors:"));
+  Serial.print(F("Init\t"));  Serial.println(Ethernet.getSocketError(0));
+  Serial.print(F("Connect\t"));  Serial.println(Ethernet.getSocketError(1));
+  Serial.print(F("State\t"));  Serial.println(Ethernet.getSocketError(2));
+
+  // wait ten seconds before asking for the time again
+  delay(10000);
+}
+
+void afficheHeure(uint32_t epoch)
+{
+  // print the hour, minute and second:
+
+  Serial.print((epoch  % 86400L) / 3600); // print the hour (86400 equals secs per day)
+  Serial.print(':');
+  if (((epoch % 3600) / 60) < 10) {
+    // In the first 10 minutes of each hour, we'll want a leading '0'
+    Serial.print('0');
+  }
+  Serial.print((epoch  % 3600) / 60); // print the minute (3600 equals secs per minute)
+  Serial.print(':');
+  if ((epoch % 60) < 10) {
+    // In the first 10 seconds of each minute, we'll want a leading '0'
+    Serial.print('0');
+  }
+  Serial.println(epoch % 60); // print the second
+}
+
+
+void afficheTemps(uint32_t temps)
+{
+  Serial.print(F("Duration = "));
+  Serial.print(temps);
+  Serial.println(F(" ms\n"));
+}
--- src/Ntp.cpp
+++ src/Ntp.cpp
@@ -1,6 +1,5 @@
 // Arduino NTP library for WizNet W5x00 based Ethernet boards
 // (c) Copyright 2020 Pierre CASAL
-
 // Released under Apache License, version 2.0
 
 #include <Arduino.h>
@@ -28,7 +27,7 @@ static tmElements_t tm;          // Cache of time elements
 void NTPClient::begin( const int timeZone, const uint16_t timeout)
 {
     nTimeZone  = timeZone;
-    nNtpTimeout = timeout;		// Waiting for an answer from the NTP server
+    nNtpTimeout = timeout;		// Waiting for an answer from the NTP server 
 }
 
 // *************************************************
@@ -36,26 +35,25 @@ uint32_t NTPClient::getEpoch(const char* NTPServer)
 {
 	IPAddress NTPAddress;
 	DNSClient dns;
-	int ret = dns.begin(Ethernet.dnsServerIP());	// Get the DNS @IP provided in Ethernet.begin(xxx))
-	if ( ret == SUCCESS ) {
+	int ret = dns.begin(Ethernet.dnsServerIP());
+	if ( ret == SUCCESS ) {		// Récupération de l'@IP du DNS déclaré dans Ethernet.begin(xxx))
 		ret = dns.getHostByName(NTPServer, NTPAddress);
 		if ( ret == SUCCESS ) {
-			//Serial.println("DNS success");
+			//Serial.print("DNS success");	Serial.println(IPAddress(NTPAddress));
 			return getEpoch(NTPAddress);
 		}
 	}
-	//Serial.print("DNS failed: ");	//Serial.println(ret);
+	//Serial.print("DNS failed: ");	Serial.println(ret);
 	return FAILED;
 }
 	
 // *************************************************
 uint32_t NTPClient::getEpoch(IPAddress& NTPAddress)
 {	
-    int result = 0;
+    uint8_t retries = 0;
     uint32_t epoch = 0;
     uint8_t packetBuffer[NTP_PACKET_SIZE];
     int packetSize = 0;
-	uint16_t operation_timeout = NTP_TIMEOUT;
     memset(packetBuffer, 0, NTP_PACKET_SIZE);
     packetBuffer[0] = 0x23;
     packetBuffer[2] = 6;
@@ -64,64 +62,63 @@ uint32_t NTPClient::getEpoch(IPAddress& NTPAddress)
     packetBuffer[13] = 0x4E;
     packetBuffer[14] = 49;
     packetBuffer[15] = 52;
-    
-    if ( nNtpTimeout < REPLY_TIMEOUT ) {		// Verify that total timeout is
-        nNtpTimeout = REPLY_TIMEOUT;			// compatible the a request timeout
-        operation_timeout = REPLY_TIMEOUT;
-    }
 	
-	uint32_t endWait = millis() + operation_timeout;
-	while ( millis() < endWait ) {
-		// Find a socket to use
-		result = ntpUdp.begin();	//		Return: 0= no socket, 1= success
-		if ( result == SUCCESS ) {
-			// Successful get socket
-			//Serial.println("ntpUdp.begin");
-            result = ntpUdp.beginPacket( NTPAddress, NTP_PORT );   // Return 1= success, 0= bad IP, no socket
+#ifdef DEBUG
+	uint32_t start = millis();
+#endif
+
+	// Find a socket to use
+    int result = nUdp.begin();
+    if ( result == SUCCESS ) {
+		// Successful get socket
+		while ( retries < MAX_RETRIES ) {
+            result = nUdp.beginPacket( NTPAddress, NTP_PORT );   // Return an int: -1= reset 5x00, 1= success, 0= fail
             if ( result == SUCCESS ) {
 				// Configure destination ip address and port OK
-				//Serial.println("ntpUdp.beginPacket");
-                ntpUdp.write(packetBuffer, NTP_PACKET_SIZE);   // Writes UDP data to the TX buffer
-                result = ntpUdp.endPacket();        // Send the packet, Returns 1= success, -1= W5x00 fail, 0= timeout
-				//Serial.print("ntpUdp.endPacket: ");	//Serial.println(result);
+                nUdp.write(packetBuffer, NTP_PACKET_SIZE);   // Writes UDP data to the TX buffer
+                result = nUdp.endPacket();        // Send the packet, Return an int: 1= send ok, -1= reset 5x00, 30= socket command failed, 31= send timeout
                 if ( result == SUCCESS ) {
 					// The packet has been sent, wait for reply..
-					//Serial.println("ntpUdp.endPacket");
 					result = TIMED_OUT;
                     uint32_t stopWait = millis() + nNtpTimeout;
                     while ( millis() < stopWait ) {
 						// Wait for a response packet
-                        packetSize = ntpUdp.parsePacket();
+                        packetSize = nUdp.parsePacket();
                         if ( packetSize >= NTP_PACKET_SIZE ) {
-							// We've got something
-							//Serial.println("ntpUdp.parsePacket");
-                            ntpUdp.read(packetBuffer, NTP_PACKET_SIZE);
-                            uint32_t secsSince1900 = 0;
-                            for (uint8_t i = 40; i < 44; i++) {
-                                secsSince1900 <<= 8;
-                                secsSince1900 |= packetBuffer[i];
-                            }
-                            epoch = secsSince1900 - SEVENTY_YEARS + NTP_LATENCY;
-                            result = SUCCESS;
-							break;		// We get something => exit
+							// We've got something, is it from who we expected ?
+							if ( ( nUdp.remoteIP() == NTPAddress ) || ( nUdp.remotePort() == NTP_PORT ) ) {
+								nUdp.read(packetBuffer, NTP_PACKET_SIZE);
+								uint32_t secsSince1900 = 0;
+								for (uint8_t i = 40; i < 44; i++) {
+									secsSince1900 <<= 8;
+									secsSince1900 |= packetBuffer[i];
+								}
+								epoch = secsSince1900 - SEVENTY_YEARS + NTP_LATENCY;
+								result = SUCCESS;
+							} else {
+								nUdp.flush();
+							}
+							break;	// exit if something received
                         }
 						delay(5);
 					}
+					if ( result == SUCCESS ) break;	// exit if SUCCESS
 				}
             }
-			ntpUdp.stop();        // Close the socket
-			if ( result == SUCCESS ) break;	// exit if SUCCESS
-			//delay(50);
+            delay(100);
+            retries++;
         }
-       delay(100);
+        // We don't need the socket anymore
+        nUdp.stop();
     }
-#if 0
+#ifdef DEBUG
 	uint32_t stop = millis();
-    //Serial.print("epoch\t");		//Serial.println(epoch);
-    //Serial.print("Elapsed\t");		//Serial.println(stop - (endWait - operation_timeout));
-    //Serial.print("Result\t");		//Serial.println(result);
+    Serial.print("epoch\t");		Serial.println(epoch);
+    Serial.print("Retries\t");		Serial.println(retries);
+    Serial.print("Elapsed\t");		Serial.println(stop - start);
+    Serial.print("Result\t");		Serial.println(result);
 #endif
-    return epoch;		// GMT time
+    return epoch;
 }
 
 
@@ -130,23 +127,23 @@ bool NTPClient::isDST(uint32_t epoch)
 {
     // Equations by Wei-Hwa Huang (US), and Robert H. van Gent (EC)
     // European Economic Community: Since 1996, valid through 2099
-	uint32_t DST_begin_epoch, DST_end_epoch;
 	
     breakTime(epoch, tm);    // breakTime gives the date corresponding to epoch(from 1970) in tm_elements
-	int year  = tm.Year;	// year contained in the given epoch is offset from 1970
-	tm.Month  = DST_BEGIN_MONTH;
-	tm.Day    = 31 - (((5 * (1970+year)) / 4) + 4) % 7;  // last sunday of march
-	tm.Hour   = DST_BEGIN_HOUR;
+
+	int year = 1970 + tm.Year;	// year contained in the given epoch is offset from 1970
+	tm.Month = DST_BEGIN_MONTH;
+	tm.Day = 31 - (((5 * year) / 4) + 4) % 7;  // last sunday of march
+	tm.Hour = DST_BEGIN_HOUR;
 	tm.Second = 0;
-	DST_begin_epoch = makeTime(tm);  // makeTime gives the corresponding epoch to DST START time
-	if ( epoch >= DST_begin_epoch ) {
-		tm.Year   = year;
-		tm.Month  = DST_END_MONTH;
-		tm.Day    = 31 - (((5 * (1970+year)) / 4) + 1) % 7;     // last sunday of october
-		tm.Hour   = DST_END_HOUR;
+	uint32_t DST_epoch = makeTime(tm);  // makeTime gives the corresponding epoch to DST START time
+	if ( epoch >= DST_epoch ) {
+		tm.Year = year - 1970;
+		tm.Month = DST_END_MONTH;
+		tm.Day = 31 - (((5 * year) / 4) + 1) % 7;     // last sunday of october
+		tm.Hour = DST_END_HOUR;
 		tm.Second = 0;
-		DST_end_epoch = makeTime(tm);  // makeTime gives the corresponding epoch to DST END time
-		if ( epoch < DST_end_epoch ) {
+		DST_epoch = makeTime(tm);  // makeTime gives the corresponding epoch to DST END time
+		if ( epoch < DST_epoch ) {
 			return true;  // Summer time
 		}
 	}
@@ -164,11 +161,11 @@ uint32_t NTPClient::localTime()
 uint32_t NTPClient::localTime(uint32_t epoch)
 {
 	if ( epoch == 0 ) return FAILED;
+    epoch += nTimeZone * 3600;    //  GMT + TIMEZONE
 	if ( isDST(epoch) == true ) {
 		epoch += 3600;   // Summer time
 	}
-    epoch += nTimeZone * 3600;    //  GMT + TIMEZONE
-    ////Serial.printf("epoch: %lu\n", epoch);
+    //Serial.printf("epoch: %lu\n", epoch);
 	return epoch;
 }
 
